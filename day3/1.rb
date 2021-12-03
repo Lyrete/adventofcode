355d8f0e451b927e9dfdd948d21a24f3a0a064d8
Tommi Alajoki <tommi@alajoki.fi>
2021-12-03 16:55:35 +0200

    Fixed value being wrong when equal amount

--- day3/1.rb
+++ day3/1.rb
@@ -3,29 +3,44 @@ data = File.readlines("data.txt").map do |line|
     line.strip.split("")
 end
 
-#flip the array
-data = data.transpose
+def find_binarystring(data)
+    #flip the array
+    data = data.transpose
 
-#Find the most common value (0 or 1) in every column (row now as we transposed the array)
-common_string = ''
-for i in 0...data.length
-    column = data[i]
-    common_value = column.group_by(&:itself).values.max_by(&:size).first
-    common_string += common_value
-end
-
-#Basically get the opposite of the most common string (1 -> 0 and 0 -> 1)
-uncommon_string = ''
-for i in 0...common_string.length
-    letter = common_string[i]
-    if letter == '0'
-        uncommon_string += '1'
-    else
-        uncommon_string += '0'
+    #Find the most common value (0 or 1) in every column (row now as we transposed the array)
+    common_string = ''
+    for i in 0...data.length
+        column = data[i]
+        ones, zeros = 0, 0
+        for letter in column
+            if letter == '1'
+                ones += 1
+            else
+                zeros += 1
+            end
+        end
+        if ones >= zeros
+            common_value = '1'
+        else
+            common_value = '0'
+        end
+        common_string += common_value
     end
+
+    #Basically get the opposite of the most common string (1 -> 0 and 0 -> 1)
+    uncommon_string = ''
+    for i in 0...common_string.length
+        letter = common_string[i]
+        if letter == '0'
+            uncommon_string += '1'
+        else
+            uncommon_string += '0'
+        end
+    end
+
+    return common_string, uncommon_string
 end
 
-puts common_string.to_i(2)
-puts uncommon_string.to_i(2)
+value1, value2 = find_binarystring data
 
-puts "Values multiplied: #{common_string.to_i(2) * uncommon_string.to_i(2)}"
+puts "Values multiplied: #{value1.to_i(2) * value2.to_i(2)}"
