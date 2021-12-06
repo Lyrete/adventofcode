6b4ed8ec6481837da6f50e7a7ecee1ebee200acd
Tommi Alajoki <tommi@alajoki.fi>
2021-12-06 18:49:23 +0200

    Refactored to hash since for loop went over limit

--- day6/1.rb
+++ day6/1.rb
@@ -1,21 +1,31 @@
 def parse_file(filename)
-    fishes = []
+    fishes = {0 => 0,1 => 0,2 => 0,3 => 0,4 => 0,5 => 0,6 => 0,7 => 0,8 => 0}
     File.readlines(filename).each do |line|
-        fishes = (line.strip.split(',').map(&:to_i))
+        fish_data = (line.strip.split(',').map(&:to_i))
+        for fish in fish_data
+            fishes[fish] += 1
+        end
     end
     return fishes
 end
 
 def pass_day(fishes)
-    for i in 0...fishes.length
-        fish = fishes[i]
-        if fish == 0
-            fishes.append(8)
-            fishes[i] = 6
-        else
-            fishes[i] -= 1
-        end
+    #Store amount of fish at 0 while we loop through the rest
+    new_fish = fishes[0]
+    fishes[0] = 0
+
+    fishes.each do |key, value|
+        if key != 0 #remove one day from each fish
+            fishes[key-1] += value
+            fishes[key] -= value        
+        end        
     end
+
+    #Return stored fish to hash
+    fishes[6] += new_fish
+    fishes[8] += new_fish
+
+    #return the hash of fishes
     return fishes
 end
 
@@ -24,6 +34,14 @@ def pass_days(days, fishes)
         fishes = pass_day fishes
     end
     return fishes
+end
+
+def count_fishes(fishes)
+    count = 0
+    fishes.each do |key, value|
+        count += value     
+    end
+    return count
 end
 
 
