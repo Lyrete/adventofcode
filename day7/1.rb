50be46eda1a9099dfb32629b1fdfbd5a4f47304a
Tommi Alajoki <tommi@alajoki.fi>
2021-12-07 16:21:30 +0200

    Changed to consider min of both ceil and floor so accurate in all cases

--- day7/1.rb
+++ day7/1.rb
@@ -6,18 +6,19 @@ data = data.sort
 mid = data[data.length/2]
 
 avg = (data.reduce(:+).to_f / data.length) #Get avg for task 2
-avg.round == avg.to_i ? avg = avg.round : avg = avg.floor #Fix the fact that .5 ending float rounds up but task needed it down
 
 
 sum1 = 0
 sum2 = 0
+sum3 = 0
 #Loop through array to get the fuel counts
 for spot in data
     sum1 += (spot - mid).abs
-    sum2 += (1..(spot - avg).abs).sum
+    sum2 += (1..(spot - avg.floor).abs).sum
+    sum3 += (1..(spot - avg.ceil).abs).sum
 end
 
 puts "Task 1:"
 p sum1
 puts "Task 2:"
-p sum2
+p [sum2, sum3].min
