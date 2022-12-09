9dfeb242c1f899f50eef4a434f5e31ac063fea4a
talajoki <tommi@alajoki.fi>
2022-12-09 19:09:07 +0200

    feat n2

--- 2022/src/bin/09.rs
+++ 2022/src/bin/09.rs
@@ -1,7 +1,8 @@
 use std::collections::HashSet;
 use std::str::FromStr;
+use std::time::Instant;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Dir {
     Right,
     Left,
@@ -23,38 +24,90 @@ impl FromStr for Dir {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Move {
     direction: Dir,
     amount: isize,
 }
 
 //Move tail towards the head
-fn move_towards(head: &(isize, isize), tail: &mut (isize, isize), dir: &Dir) {
-    let c_dist = (tail.0 - head.0).abs().max((tail.1 - head.1).abs());
+fn move_towards(head: (isize, isize), tail: &mut (isize, isize)) {
+    let dy = head.0 - tail.0;
+    let dx = tail.1 - head.1;
+
+    let c_dist = dy.abs().max(dx.abs());
 
     if c_dist <= 1 {
         return;
     }
 
-    if head.0 < tail.0 && *dir == Dir::Left {
-        tail.0 = head.0 + 1;
+    let edges = HashSet::from([
+        (head.0, head.1 - 1),
+        (head.0, head.1 + 1),
+        (head.0 + 1, head.1),
+        (head.0 - 1, head.1),
+    ]);
+
+    let mut dest = tail.clone();
+
+    for edge in edges.iter() {
+        let dy = edge.0 - tail.0;
+        let dx = edge.1 - tail.1;
+
+        let c_dist = dy.abs().max(dx.abs());
+        if c_dist == 1 {
+            dest = *edge;
+        }
     }
 
-    if head.0 > tail.0 && *dir == Dir::Right {
-        tail.0 = head.0 - 1;
+    //Seems like we found an edge close enough
+    if dest != *tail {
+        *tail = dest;
+        return;
     }
 
-    if head.1 < tail.1 && *dir == Dir::Down {
-        tail.1 = head.1 + 1;
+    let corners = HashSet::from([
+        (head.0 - 1, head.1 - 1),
+        (head.0 - 1, head.1 + 1),
+        (head.0 + 1, head.1 + 1),
+        (head.0 + 1, head.1 - 1),
+    ]);
+
+    let mut dest = tail.clone();
+
+    for edge in corners.iter() {
+        let dy = edge.0 - tail.0;
+        let dx = edge.1 - tail.1;
+
+        let c_dist = dy.abs().max(dx.abs());
+        if c_dist == 1 {
+            dest = *edge;
+        }
     }
 
-    if head.1 > tail.1 && *dir == Dir::Up {
-        tail.1 = head.1 - 1;
+    //Seems like we found an edge close enough
+    *tail = dest;
+}
+
+fn move_head(head: &mut (isize, isize), dir: Dir) {
+    let amt = if dir == Dir::Left || dir == Dir::Down {
+        -1
+    } else {
+        1
+    };
+
+    if dir == Dir::Left || dir == Dir::Right {
+        head.0 += amt;
+    }
+
+    if dir == Dir::Up || dir == Dir::Down {
+        head.1 += amt;
     }
 }
 
 fn main() {
+    let start = Instant::now();
+
     let input: Vec<_> = include_str!("./data9.txt")
         .lines()
         .map(|c| {
@@ -67,61 +120,23 @@ fn main() {
         })
         .collect();
 
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
-
     let mut visited: HashSet<(isize, isize)> = HashSet::new();
-    visited.insert(tail);
+
+    let mut knots = vec![(0, 0); 10];
+    let mut visited2: HashSet<(isize, isize)> = HashSet::new();
 
     for move_daddy in input.iter() {
-        if move_daddy.direction == Dir::Right {
-            head.0 += move_daddy.amount;
-            for _ in 0..move_daddy.amount {
-                let c_dist = (tail.0 - head.0).abs().max((tail.1 - head.1).abs());
-                if c_dist > 1 {
-                    tail.1 = head.1;
-                    tail.0 += 1;
-                    visited.insert(tail);
-                }
+        for _ in 0..move_daddy.amount {
+            move_head(&mut knots[0], move_daddy.direction);
+            for idx in 1..knots.len() {
+                move_towards(knots[idx - 1], &mut knots[idx]);
             }
-        }
-
-        if move_daddy.direction == Dir::Left {
-            head.0 -= move_daddy.amount;
-            for _ in 0..move_daddy.amount {
-                let c_dist = (tail.0 - head.0).abs().max((tail.1 - head.1).abs());
-                if c_dist > 1 {
-                    tail.1 = head.1;
-                    tail.0 -= 1;
-                    visited.insert(tail);
-                }
-            }
-        }
-
-        if move_daddy.direction == Dir::Up {
-            head.1 += move_daddy.amount;
-            for _ in 0..move_daddy.amount {
-                let c_dist = (tail.0 - head.0).abs().max((tail.1 - head.1).abs());
-                if c_dist > 1 {
-                    tail.0 = head.0;
-                    tail.1 += 1;
-                    visited.insert(tail);
-                }
-            }
-        }
-
-        if move_daddy.direction == Dir::Down {
-            head.1 -= move_daddy.amount;
-            for _ in 0..move_daddy.amount {
-                let c_dist = (tail.0 - head.0).abs().max((tail.1 - head.1).abs());
-                if c_dist > 1 {
-                    tail.0 = head.0;
-                    tail.1 -= 1;
-                    visited.insert(tail);
-                }
-            }
+            visited.insert(knots[1]);
+            visited2.insert(*knots.last().unwrap());
         }
     }
 
     println!("Task 1: {:?}", visited.len());
+    println!("Task 2: {:?}", visited2.len());
+    println!("Elapsed: {:.2?}", start.elapsed());
 }
