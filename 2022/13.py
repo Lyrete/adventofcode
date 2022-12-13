597db11b6496af37b0d46299b3d75ba5aee97cdc
talajoki <tommi@alajoki.fi>
2022-12-13 23:07:05 +0200

    optimizing the list read a bit

--- 2022/13.py
+++ 2022/13.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from functools import cmp_to_key
 
 def compare(a, b):
@@ -16,27 +15,19 @@ def compare(a, b):
         return compare(a,[b])
     
 
-txt = Path('inputs/data13.txt').read_text().split("\n\n")
-
-pairs = []
-
-for chunk in txt:
-    litarray = chunk.rstrip().split("\n")
-    left = eval(litarray[0])
-    right = eval(litarray[1])
-    pairs.append((left, right))
+lists = list(map(eval, open('inputs/data13.txt').read().strip().split()))
 
 correct = 0
 
-for (i, pair) in enumerate(pairs):
-    if compare(pair[0], pair[1]) < 1:
-        correct += i + 1
+for i in range(1, len(lists), 2):
+    l = lists[i-1]
+    r = lists[i]
+    if compare(l, r) < 1:
+        correct += 1 + i // 2 
 
-flattened = [elem for tuple in pairs for elem in tuple]
-flattened.append([[2]])
-flattened.append([[6]])
+lists = lists + [[[2]]] + [[[6]]]
 
-flattened.sort(key=cmp_to_key(compare))
+lists.sort(key=cmp_to_key(compare))
 
 print("Task 1:", correct)
-print("Task 2:", (flattened.index([[2]]) + 1) * (flattened.index([[6]]) + 1))
+print("Task 2:", (lists.index([[2]]) + 1) * (lists.index([[6]]) + 1))
